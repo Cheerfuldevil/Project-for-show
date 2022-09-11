3ceeb9830d00f5e4d1c8abc62e52a67594981340
Cheerfuldevil <Cheerfuldevil13@yandex.ru>
2022-09-11 22:58:26 +0300

    Added 4 steps of first test case.

---
+++ Object Repository/GoogleMainPage/AcceptAllButton.rs
@@ -0,0 +1,123 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>AcceptAllButton</name>
+   <tag></tag>
+   <elementGuidId>0771ffba-adba-4417-a753-50ce75eabbee</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>#L2AGLb > div.QS5gu.sy4vM</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>id(&quot;L2AGLb&quot;)/div[@class=&quot;QS5gu sy4vM&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>05424698-59c0-4117-a914-a150cff12108</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>QS5gu sy4vM</value>
+      <webElementGuid>21ecc839-3203-48a9-b71d-e33c0796fc56</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>role</name>
+      <type>Main</type>
+      <value>none</value>
+      <webElementGuid>f2807dba-0563-49bf-9ab9-b5d391fb8ef2</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Принять все</value>
+      <webElementGuid>ceb76782-fb3d-49b0-a638-81777e8a3881</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;L2AGLb&quot;)/div[@class=&quot;QS5gu sy4vM&quot;]</value>
+      <webElementGuid>01347419-0ae4-478c-9319-7725fe48417b</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//button[@id='L2AGLb']/div</value>
+      <webElementGuid>a9d33da0-b10d-4cc0-ab45-e8d01ac84692</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Отклонить все'])[1]/following::div[1]</value>
+      <webElementGuid>b55b1d38-ded3-4f01-a0a0-430eef5a2928</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='g.co/privacytools'])[1]/following::div[4]</value>
+      <webElementGuid>8c6792dc-83ab-496c-b096-b3607a374ae4</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Другие варианты'])[1]/preceding::div[1]</value>
+      <webElementGuid>5b993f6d-d5fa-4ab1-bb5d-383ed4854f6a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Конфиденциальность'])[2]/preceding::div[2]</value>
+      <webElementGuid>117f5fab-d7f6-4d24-8c0a-a9e837ad2bca</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Принять все']/parent::*</value>
+      <webElementGuid>44af8aa8-7fcc-46a8-94b6-01991714cf52</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//button[2]/div</value>
+      <webElementGuid>10e07774-2bc7-403d-bbf2-0cdede544ffb</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = 'Принять все' or . = 'Принять все')]</value>
+      <webElementGuid>91afb463-5ed2-46b5-9fae-9eb9fbdfccbb</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
